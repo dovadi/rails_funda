b1184fae89671692ebfa550b8b52dd74818cf315
rkyrychuk <ruslan.kyrychuk@gmail.com>
2008-09-22 16:58:50 +0300

    Fixed first user create bug (Parenthes missing caused null reference).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     user = User.find_by_activation_code(params[:activation_code]) unless params[:activation_code].blank?
     case
     when (!params[:activation_code].blank?) && user && !user.active?
-      user.roles << Role.find_by_name('admin') or Role.create!(:name=>'admin') if User.count == 1
+      user.roles << (Role.find_by_name('admin') or Role.create!(:name=>'admin')) if User.count == 1
       user.activate!
       flash[:notice] = "Signup complete! Please sign in to continue."
       redirect_to '/login'
