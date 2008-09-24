5fff37b0aac31ceeb5d72781c2ccabd1779d3cb7
kiosan <obondar@gmail.com>
2008-09-24 11:38:28 +0300

    added link to IE and fixed error in FF

--- public/javascripts/jquery.ie6blocker.js
+++ public/javascripts/jquery.ie6blocker.js
@@ -1,6 +1,7 @@
 var ieVer = navigator.userAgent.match(/MSIE (\d+\.\d+)/);
 //var IE6 = (navigator.userAgent.indexOf("MSIE 6")>=0) ? true : false;
-var IE6 = parseFloat(ieVer[1]) < 7;
+var IE6 = ieVer ? parseFloat(ieVer[1]) < 7 : false;
+
 if(IE6){
 
 	$(function(){
@@ -18,7 +19,7 @@ if(IE6){
 				zIndex: 5000
 			})
 			.appendTo(document.body);
-		$("<div><img src='/images/no-ie6.png' alt='' style='float: left;'/><p><br /><strong>Sorry! This page doesn't support Internet Explorer 6.</strong><br /><br />If you'd like to read our content please <a href='http://getfirefox.com'>upgrade your browser</a>.</p>")
+		$("<div><img src='/images/no-ie6.png' alt='' style='float: left;'/><p><br /><strong>Sorry! This page doesn't support Internet Explorer 6.</strong><br /><br />If you'd like to read our content please <a href='http://www.microsoft.com/windows/downloads/ie/getitnow.mspx'>upgrade IE</a> or install <a href='http://getfirefox.com'>Firefox</a>.</p>")
 			.css({
 				backgroundColor: 'white',
 				'left': '50%',
