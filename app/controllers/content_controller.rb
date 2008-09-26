cec5196b576e5a66b20cf63eb59cd70f6369b2c5
Frank Oxener <frank.oxener@gmail.com>
2008-09-26 15:59:09 +0200

    Cleaned up index method

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,5 +1,4 @@
-  class ContentController < ApplicationController
+class ContentController < ApplicationController
   def index
-    #redirect_to home_url(:subdomain=>current_user.login) if current_user
   end
 end
